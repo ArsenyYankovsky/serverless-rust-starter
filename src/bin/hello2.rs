e38ed6368482a149dd0c1d610b8f5fcd63860e3c
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2023-07-31 21:49:46 +0200

    fix(*): added a second lambda as an example

---
+++ src/bin/hello2.rs
@@ -0,0 +1,23 @@
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use lambda_runtime::{LambdaEvent, service_fn};
+
+type Error = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    lambda_runtime::run(service_fn(handler)).await?;
+    Ok(())
+}
+
+async fn handler(event: LambdaEvent<ApiGatewayProxyRequest>) -> Result<ApiGatewayProxyResponse, Error> {
+    Ok(ApiGatewayProxyResponse {
+        status_code: 200,
+        headers: Default::default(),
+        multi_value_headers: Default::default(),
+        body: serde_json::to_string(&event.payload)
+            .ok()
+            .map(|v| Body::Text(v)),
+        is_base64_encoded: false,
+    })
+}
